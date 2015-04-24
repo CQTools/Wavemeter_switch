a1bf30d867a551f69284b686d7a5a04f00ad183c
general lab user <qitlab@charsiew.qoptics.quantum.nus.edu.sg>
2015-04-24 14:18:54 +0800

    added serial_port_reader_file

--- main.py
+++ main.py
@@ -133,14 +133,3 @@ def get_frequency(webdata):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080)
-
-
-
-Ciao...
-wavemeterswitch
-Ciao...
-wavemeterswitch
-Ciao...
-wavemeterswitch
-Ciao...
-wavemeterswitch
